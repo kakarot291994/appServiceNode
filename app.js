163b0280eaddce9440df473d78cf18ee291bc6aa
kakarot291994 <amolchh1994@gmail.com>
2026-02-10 07:36:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log('Sucess..!')
-  res.json({ message: 'Hello, From Application Hosted on App Services , This is my live application running on version2' })
+  res.json({ message: 'Hello, From Application Hosted on App Services. })
 });
 
 
